7caacbf1d16b0b7f2e59677ebef131bd063a2610
Benjamineidum <benjamineidum@gmail.com>
2022-02-06 15:36:57 -0500

    Add License link function

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -23,17 +23,128 @@ const renderLicenseBadge = licenseName => {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const generateLicense = LicenseText => {
+  if (!LicenseText) {
+    return '';
+  }
+  return `# License
+    
+${LicenseText}
+    `;
+};
 
-// TODO: Create a function that returns the license section of README
+// create the badges section
+const generateBadges = badgesText => {
+  if (!badgesText) {
+    return '';
+  }
+  return `# Badges
+    
+${badgesText}
+    `;
+};
+// create the features section
+const generateFeatures = featuresText => {
+  if (!featuresText) {
+    return '';
+  }
+  return `# Features
+    
+${featuresText}
+    `;
+};
+// create the Contributing section
+const generateContributing = contributingText => {
+  if (!contributingText) {
+    return '';
+  }
+  return `# Contributing
+    
+${contributingText}
+    `;
+};
+// create the tests section
+const generateTests = testsText => {
+  if (!testsText) {
+    return '';
+  }
+  return `# Tests
+    
+${testsText}
+    `;
+};
+
+// create the table of contents section
+const generateTableOfContents = tableOfContents => {
+  if (tableOfContents === false) {
+    return '';
+  }
+  return `# Table of Contents
+    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Questions](#questions)`;
+};
+// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+const tableOfContentsLicense = license => {
+  if (!license) {
+    return '';
+  }
+  return `* [License](#license)`
+}
+const tableOfContentsBadges = badges => {
+  if (!badges) {
+    return '';
+  }
+  return `* [Badges](#badges)`
+}
+const tableOfContentsFeatures = features => {
+  if (!features) {
+    return '';
+  }
+  return `* [Features](#features)`
+}
+const tableOfContentsContributing = contributing => {
+  if (!contributing) {
+    return '';
+  }
+  return `* [Contributing](#contributing)`
+}
+const tableOfContentsTests = tests => {
+  if (!tests) {
+    return '';
+  }
+  return `* [Tests](#tests)`
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown => {
+  const { projectName, description, installation, usage, credits, github, email, ...notRequired } = generateMarkdown;
+  //console.log(generateMarkdown);
+
+  return `# ${projectName}
+    ${renderLicenseBadge(notRequired.license)}
+${description}
+${generateTableOfContents(notRequired.confirmTableOfContents)}
+${tableOfContentsLicense(notRequired.license)}
+${tableOfContentsBadges(notRequired.badges)}
+${tableOfContentsFeatures(notRequired.features)}
+${tableOfContentsContributing(notRequired.contributing)}
+${tableOfContentsTests(notRequired.tests)}
+# Installation
+${installation}
+# Usage 
+${usage}
+# Credits
+${credits}
+# Questions
+[Contact Me](${email})
+[GitHub](https://github.com/${github})
+${generateLicense(notRequired.license)}
+${generateBadges(notRequired.badges)}
+${generateFeatures(notRequired.features)}
+${generateContributing(notRequired.contributing)}
+${generateTests(notRequired.tests)}
+`;
+}
